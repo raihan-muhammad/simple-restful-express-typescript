de277a66ab4cf1d62a28268faa85a58c84fa724c
raihanmuhammad <michael.raihan@stockbit.com>
2022-03-20 16:10:08 +0700

    feat: add dotenv configuration on entry point

--- src/index.ts
+++ src/index.ts
@@ -4,33 +4,35 @@ import morgan from 'morgan';
 import compression from 'compression';
 import helmet from 'helmet';
 import cors from 'cors';
+import { config as dotenv } from "dotenv";
 
 // Routes
-import UserRoutes from './routers/UserRoutes';
+import UserRoutes from "./routers/UserRoutes";
 
 class App {
-	public app: Application;
-	constructor(){
-		this.app = express();
-		this.plugins();
-		this.routes();
-	}
+  public app: Application;
+  constructor() {
+    this.app = express();
+    this.plugins();
+    this.routes();
+    dotenv();
+  }
 
-	protected plugins(): void {
-		this.app.use(bodyParser.json());
-		this.app.use(morgan("dev"));
-		this.app.use(compression());
-		this.app.use(helmet());
-		this.app.use(cors());
-	}
+  protected plugins(): void {
+    this.app.use(bodyParser.json());
+    this.app.use(morgan("dev"));
+    this.app.use(compression());
+    this.app.use(helmet());
+    this.app.use(cors());
+  }
 
-	protected routes(): void {
-		this.app.use("/api/v1/users", UserRoutes);
-	}
+  protected routes(): void {
+    this.app.use("/api/v1/users", UserRoutes);
+  }
 }
 
 const port: number = 8000;
 const app = new App().app;
 app.listen(port, () => {
-	console.log(`Applcation running on port ${port}`)
+  console.log(`Applcation running on port ${port}`);
 });
